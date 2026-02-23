4939b31584bd06ae2f3d0af6c50ab7b96c68a03f
Xavi Guasch <xaviguasch@gmail.com>
2026-02-23 11:47:52 +0100

    added Header changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,18 +8,26 @@ function Header() {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className="container pt-8 ">
+    <div>
       <div className="mb-[48px] flex justify-between items-center">
-        <img src={theme === "dark" ? darkLogo : lightLogo} />
+        <div className="w-[185px] h-[30px] md:w-[245px] md:h-[40px]">
+          <img
+            src={theme === "dark" ? darkLogo : lightLogo}
+            className="w-full h-full"
+          />
+        </div>
         <div
           onClick={toggleTheme}
-          className="w-[44px] h-[44px] rounded-lg flex items-center justify-center bg-bg-secondary"
+          className="w-[32px] h-[32px] md:w-[44px] md:h-[44px] rounded-sm mdrounded-lg flex items-center justify-center bg-background2"
         >
-          <img src={theme === "dark" ? iconSun : iconMoon} />
+          <img
+            src={theme === "dark" ? iconSun : iconMoon}
+            className="w-[20px] h-[20px] md:w-[22px] md:h-[22px]"
+          />
         </div>
       </div>
-      <h1 className="text-text1 text-preset-1 text-center mb-12">
-        Analyze your text <span className="block">in real-time</span>
+      <h1 className="text-preset-1 text-text1 text-center mb-12 ">
+        Analyze your text <span className="inline md:block">in real-time</span>
       </h1>
     </div>
   );
