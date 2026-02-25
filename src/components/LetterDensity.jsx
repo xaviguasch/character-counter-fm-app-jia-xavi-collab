46aacc0fe3c5bf33d1aa674d66d59196f0bea141
Xavi Guasch <xaviguasch@gmail.com>
2026-02-25 07:31:34 +0100

    added last batch of changes to LetterDensity

--- src/components/LetterDensity.jsx
+++ src/components/LetterDensity.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
+
 import { getLetterPercentages } from "../utils/utils";
 import LetterGroup from "./LetterGroup";
 
 function LetterDensity({ text }) {
+  const [showAll, setShowAll] = useState(false);
+
   let letterGroupDataArr;
 
   if (text) {
@@ -9,28 +13,34 @@ function LetterDensity({ text }) {
     letterGroupDataArr = Object.entries(getLetterPercentages(text));
   }
 
+  const sorted = letterGroupDataArr?.slice().sort((a, b) => b[1][0] - a[1][0]);
+
+  const visibleItems = showAll ? sorted : sorted?.slice(0, 5);
+
   return (
     <div className="flex flex-col gap-5">
       <h2 className="text-preset-2">Letter Density</h2>
 
       <div className="flex flex-col gap-3">
         {text &&
-          letterGroupDataArr
-            // we're sorting for higher frequency to lower
-            .sort((a, b) => b[1][0] - a[1][0])
-            .map((lgArr, idx) => {
-              const [letter, [count, percentage]] = lgArr;
-
-              return (
-                <LetterGroup
-                  key={idx}
-                  letter={letter}
-                  count={count}
-                  percentage={percentage}
-                />
-              );
-            })}
+          visibleItems?.map(([letter, [count, percentage]]) => (
+            <LetterGroup
+              key={letter}
+              letter={letter}
+              count={count}
+              percentage={percentage}
+            />
+          ))}
       </div>
+
+      {text && sorted?.length > 5 && (
+        <button
+          onClick={() => setShowAll((prev) => !prev)}
+          className="text-preset-3 text-left cursor-pointer"
+        >
+          {showAll ? "See less" : "See more"}
+        </button>
+      )}
     </div>
   );
 }
